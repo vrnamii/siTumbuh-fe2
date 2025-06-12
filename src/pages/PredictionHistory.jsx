c83bed9dda95e69173f5d5c78fcf7504dae5af68
Ni KOmang Purnami <nikomangpurnami8@gmail.com>
2025-06-12 15:35:59 +0800

    mengubah warna PredictionHistory

--- src/pages/PredictionHistory.jsx
+++ src/pages/PredictionHistory.jsx
@@ -40,7 +40,7 @@ export default function PredictionHistory() {
   return (
     <div className="min-h-screen mt-32 mb-24 px-2 md:px-16">
       <h2 className="text-center text-[var(--color-blue)] text-2xl font-semibold mb-10">Riwayat Periksa</h2>
-      <p className="text-sm sm:text-base text-center text-gray-500 mb-10 px-2 sm:px-6 md:px-16 lg:px-100 ">
+      <p className="text-sm sm:text-base text-center text-[var(--color-darkgrey)] mb-10 px-2 sm:px-6 md:px-16 lg:px-100 ">
           Lorem ipsum dolor sit amet consectetur. At sagittis semper duis sed curabitur vitae id enim diam.
           Condimentum ultricies nunc mauris egestas leo tempus congue dictum.
       </p>
@@ -50,13 +50,13 @@ export default function PredictionHistory() {
           placeholder="Cari nama..."
           value={search}
           onChange={e => setSearch(e.target.value)}
-          className="border border-gray-300 rounded-md px-3 py-2 w-full max-w-xs focus:outline-none focus:ring-2 focus:ring-sky-400"
+          className="border border-[var(--color-darkgrey)] rounded-md px-3 py-2 w-full max-w-xs focus:outline-none focus:ring-2 focus:ring-[var(--color-blue)]"
         />
       </div>
       <div className="w-full overflow-x-auto">
         <table className="min-w-full table-auto border-collapse rounded-lg overflow-hidden">
           <thead>
-            <tr className="text-sm sm:text-base bg-sky-400 text-white text-left">
+            <tr className="text-sm sm:text-base bg-[var(--color-blue)] text-white text-left">
               <th className="p-3">Nama</th>
               <th className="p-3">Tanggal</th>
               <th className="p-3">Umur (bulan)</th>
@@ -64,10 +64,10 @@ export default function PredictionHistory() {
               <th className="p-3 min-w-[120px] sm:min-w-[200px]">Hasil Prediksi</th>
             </tr>
           </thead>
-          <tbody className="text-gray-700 bg-gray-100">
+          <tbody className="text-black bg-[var(--color-grey)]">
             {filteredData.length === 0 ? (
               <tr>
-                <td colSpan={5} className="text-sm sm:text-base text-center py-6 text-gray-400">Tidak ada data ditemukan</td>
+                <td colSpan={5} className="text-sm sm:text-base text-center py-6 text-[var(--color-darkgrey)]">Tidak ada data ditemukan</td>
               </tr>
             ) : (
               filteredData.map((item, idx) => (
@@ -79,7 +79,7 @@ export default function PredictionHistory() {
                   <td className="text-sm sm:text-base p-3 min-w-[160px] sm:min-w-[180px]">
                     <span
                       className={`px-3 py-1 rounded-full text-white text-xs font-medium sm:text-base ${
-                        item.hasil === "Stunting" ? "bg-red-500" : "bg-green-400"
+                        item.hasil === "Stunting" ? "bg-[var(--color-red)]" : "bg-[var(--color-green)]"
                       }`}
                     >
                       {item.hasil}
